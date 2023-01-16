31ab4c0b8c1ff9e8c6e499225a29ec36b31c4b98
ArtemShuhat <shuhat.artem2006@gmail.com>
2023-01-16 16:41:35 +0200

    scrolling

---
+++ script.js
@@ -0,0 +1,162 @@
+"use strict";
+function QuickReview() {
+  for (let i = 0; i < 50; i++) {
+    document.documentElement.scrollTop = 5900;
+  }
+}
+let Quick = document.getElementById("QuickReview");
+Quick.addEventListener("click", QuickReview);
+
+let img = document.getElementById("n");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop < 50) {
+    img.src = "images/Mercedes1.jpg";
+  }
+  if (document.documentElement.scrollTop > 200) {
+    img.src = "images/Mercedes2.jpg";
+  }
+  if (document.documentElement.scrollTop > 300) {
+    img.src = "images/Mercedes3.jpg";
+  }
+  if (document.documentElement.scrollTop > 400) {
+    img.src = "images/Mercedes4.jpg";
+  }
+  if (document.documentElement.scrollTop > 500) {
+    img.src = "images/Mercedes5.jpg";
+  }
+  if (document.documentElement.scrollTop > 600) {
+    img.src = "images/Mercedes6.jpg";
+  }
+  if (document.documentElement.scrollTop > 700) {
+    img.src = "images/Mercedes7.jpg";
+  }
+  if (document.documentElement.scrollTop > 800) {
+    img.src = "images/Mercedes8.jpg";
+  }
+  if (document.documentElement.scrollTop > 900) {
+    img.src = "images/Mercedes9.jpg";
+  }
+  if (document.documentElement.scrollTop > 1000) {
+    img.src = "images/Mercedes10.jpg";
+  }
+  if (document.documentElement.scrollTop > 1050) {
+    img.src = "images/Mercedes11.jpg";
+  }
+  if (document.documentElement.scrollTop > 1200) {
+    img.src = "images/Mercedes12.jpg";
+  }
+  if (document.documentElement.scrollTop > 1300) {
+    img.src = "images/Mercedes13.jpg";
+  }
+  if (document.documentElement.scrollTop > 1400) {
+    img.src = "images/Mercedes14.jpg";
+  }
+  if (document.documentElement.scrollTop > 1500) {
+    img.src = "images/Mercedes15.jpg";
+  }
+  if (document.documentElement.scrollTop > 1600) {
+    img.src = "images/Mercedes16.jpg";
+  }
+  if (document.documentElement.scrollTop > 1700) {
+    img.src = "images/Mercedes17.jpg";
+  }
+  if (document.documentElement.scrollTop > 1800) {
+    img.src = "images/Mercedes18.jpg";
+  }
+  if (document.documentElement.scrollTop > 1900) {
+    img.src = "images/Mercedes19.jpg";
+  }
+  if (document.documentElement.scrollTop > 2100) {
+    img.src = "images/Mercedes20.jpg";
+  }
+  if (document.documentElement.scrollTop > 2200) {
+    img.src = "images/Mercedes21.jpg";
+  }
+  if (document.documentElement.scrollTop > 2300) {
+    img.src = "images/Mercedes22.jpg";
+  }
+  if (document.documentElement.scrollTop > 2400) {
+    img.src = "images/Mercedes23.jpg";
+  }
+  if (document.documentElement.scrollTop > 2500) {
+    img.src = "images/Mercedes23.jpg";
+  }
+  if (document.documentElement.scrollTop > 2600) {
+    img.src = "images/Mercedes24.jpg";
+  }
+  if (document.documentElement.scrollTop > 2700) {
+    img.src = "images/Mercedes25.jpg";
+  }
+  if (document.documentElement.scrollTop > 2800) {
+    img.src = "images/Mercedes26.jpg";
+  }
+  if (document.documentElement.scrollTop > 2900) {
+    img.src = "images/Mercedes27.jpg";
+  }
+  if (document.documentElement.scrollTop > 3000) {
+    img.src = "images/Mercedes28.jpg";
+  }
+  if (document.documentElement.scrollTop > 3100) {
+    img.src = "images/Mercedes29.jpg";
+  }
+  if (document.documentElement.scrollTop > 3200) {
+    img.src = "images/Mercedes30.jpg";
+  }
+  if (document.documentElement.scrollTop > 3300) {
+    img.src = "images/Mercedes31.jpg";
+  }
+  if (document.documentElement.scrollTop > 3400) {
+    img.src = "images/Mercedes32.jpg";
+  }
+  if (document.documentElement.scrollTop > 3500) {
+    img.src = "images/Mercedes33.jpg";
+  }
+  if (document.documentElement.scrollTop > 3600) {
+    img.src = "images/Mercedes34.jpg";
+  }
+  if (document.documentElement.scrollTop > 3700) {
+    img.src = "images/Mercedes36.jpg";
+  }
+  if (document.documentElement.scrollTop > 3800) {
+    img.src = "images/Mercedes37.jpg";
+  }
+  if (document.documentElement.scrollTop > 3900) {
+    img.src = "images/Mercedes38.jpg";
+  }
+  if (document.documentElement.scrollTop > 4000) {
+    img.src = "images/Mercedes39.jpg";
+  }
+  if (document.documentElement.scrollTop > 4100) {
+    img.src = "images/Mercedes40.jpg";
+  }
+  if (document.documentElement.scrollTop > 4200) {
+    img.src = "images/Mercedes41.jpg";
+  }
+  if (document.documentElement.scrollTop > 4300) {
+    img.src = "images/Mercedes42.jpg";
+  }
+  if (document.documentElement.scrollTop > 4400) {
+    img.src = "images/Mercedes43.jpg";
+  }
+  if (document.documentElement.scrollTop > 4500) {
+    img.src = "images/Mercedes44.jpg";
+  }
+  if (document.documentElement.scrollTop > 4600) {
+    img.src = "images/Mercedes45.jpg";
+  }
+  if (document.documentElement.scrollTop > 4700) {
+    img.src = "images/Mercedes46.jpg";
+  }
+  if (document.documentElement.scrollTop > 4800) {
+    img.src = "images/Mercedes47.jpg";
+  }
+  if (document.documentElement.scrollTop > 4900) {
+    img.src = "images/Mercedes48.jpg";
+  }
+  if (document.documentElement.scrollTop > 5000) {
+    img.src = "images/Mercedes49.jpg";
+  }
+  if (document.documentElement.scrollTop > 5100) {
+    img.src = "images/Mercedes50.jpg";
+  }
+};
